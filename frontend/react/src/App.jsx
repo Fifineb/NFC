22a171c2de0fe5cd40f4a1856d7ec66b7c7c5b8d
Feriel Nebbache <nebbacheferiel471@gmail.com>
2026-02-27 07:09:51 +0100

    Creation de React

---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+function App() {
+  return (
+    <div>
+      <h1>Gestion de Stock</h1>
+      <p className="app-title">Smart Stock Management</p>
+    </div>
+  );
+}
+
+export default App;
