01cd90dc4b6c512980aecc813b8f16dcf9e41941
Feriel Nebbache <nebbacheferiel471@gmail.com>
2026-03-06 18:46:38 +0100

    Homepage

---
+++ frontend/react/src/Pages/public/Home.jsx
@@ -0,0 +1,14 @@
+
+function Home() {
+    
+    return (
+        <div>
+            <h1>Bienvenue sur Smart Stock</h1>
+            <p>Gestion efficace de votre inventaire</p>
+        </div>
+    );
+}
+
+export default Home;
+
+
